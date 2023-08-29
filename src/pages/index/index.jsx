b92b924c99971bedbba9000132a79f4db26cb15c
jiangyi <jiangyi@corp-ci.com>
2023-08-29 17:06:21 +0800

    finish

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -4,24 +4,18 @@ import './index.less'
 import { useState, useEffect } from 'react'
 
 
-const buildingList = ['金基璟樾府', '新睿樾府', '雅玥']
-const timeList = ['上午', '下午']
+
 export default function Index () {
   const [current, setCurrent] = useState(0)
-  const [curBuilding, setCurBuilding] = useState('')
-  const [date, setDate] = useState('')
-  const [time, setTime] = useState('')
-  const [name, setName] = useState('')
-  const [phone, setPhone] = useState('')
-  const [renshu, setRenshu] = useState('')
 
-  useShareAppMessage((res)=>{
-      console.log('onShareAppMessage', res)
-      return {
-        title: "金基业主会所体验预约",
-        path: "/pages/index/index",
-        imageUrl: "http://pic.buzhizhe.cn/o_1h905jpu31kkds2e1ia3150r1bcqa.jpg",
-      };
+
+  useShareAppMessage((res) => {
+    console.log('onShareAppMessage', res)
+    return {
+      title: "金基业主会所体验预约",
+      path: "/pages/index/index",
+      imageUrl: "http://pic.buzhizhe.cn/o_1h905jpu31kkds2e1ia3150r1bcqa.jpg",
+    };
   })
 
   useLoad(() => {
@@ -30,18 +24,7 @@ export default function Index () {
   const clickHandler = () => {
     setCurrent(current + 1)
   }
-  const catchTouchMove = () => {
-    return false;
-  }
-  const onBuildingChange = (e) => {
-    setCurBuilding(buildingList[e.detail.value])
-  }
-  const onTimeChange = (e) => {
-    setTime(timeList[e.detail.value])
-  }
-  const onDateChange = e => {
-    setDate(e.detail.value)
-  }
+
 
   const jumpYuYue = async (addresss) => {
     redirectTo({
@@ -52,20 +35,44 @@ export default function Index () {
     setCurrent(e.target.current)
   }
   const onTransition = (e) => {
-      return false
+    return false
   }
+  const touchEndHandler = (e,b,c) => {
+    const length = 4
+    const show_index = current
 
+    console.log('touchEndHandler', e,b,c)
+    // if (endY > locationY) { //下拉，回到前一页
+    //   if (index == 0) { //禁止从内容第一页回到首页
+    //     _this.setData({
+    //       show_index: index
+    //     })
+    //     return;
+    //   }
+    //   //下面是下拉事件的相关代码，这里就不添出来了。
+    // }
+    // else { //上拉，前往下一页
+    //   if (index == (length + 1)) {  //禁止从尾页 前往 首页
+    //     _this.setData({
+    //       show_index: index
+    //     })
+    //     return;
+    //   }
+    //   //下面是上拉事件的相关代码，这里就不添出来了。
+    // }
+  }
   return (
     <View className='root'>
       <View className='container'>
         <Swiper
           onTouchMove={() => { return false }}
+          onTouchEnd={touchEndHandler}
           current={current}
           className='swiper-root'
           indicatorColor='#999'
           indicatorActiveColor='#333'
           vertical
-          circular
+          // circular
           disableTouch={true}
           disableTouchmove
           onChange={onSwoperChange}
@@ -131,20 +138,20 @@ export default function Index () {
                 className='content'
                 src=' http://pic.buzhizhe.cn/o_1h8tp2iap4lhcp7qkq18rd14kda.png'
               />
-           {/*  ['金基璟樾府', '新睿樾府', '雅玥'] */}
+              {/*  ['金基璟樾府', '新睿樾府', '雅玥'] */}
 
               <Image
-                onClick={()=>{jumpYuYue('金基璟樾府')}}
+                onClick={() => { jumpYuYue('金基璟樾府') }}
                 className='top animate__pulse'
                 src='http://pic.buzhizhe.cn/o_1h8u0vcphoad5dr1eto8l41gvle.png'
               />
               <Image
-                onClick={()=>{jumpYuYue('新睿樾府')}}
+                onClick={() => { jumpYuYue('新睿樾府') }}
                 className='middle animate__pulse'
                 src='http://pic.buzhizhe.cn/o_1h8u135mrnivah4c211qv33ua.png'
               />
               <Image
-                onClick={()=>{jumpYuYue('雅玥')}}
+                onClick={() => { jumpYuYue('雅玥') }}
                 className='bottom animate__pulse'
                 src='http://pic.buzhizhe.cn/o_1h8u128r314dp1uoppg712fdaasa.png'
               />
